0c6d7e920489cd6a8fcc721fd18c00f98cf16298
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-07-19 23:07:18 -0300

    exercicio 05 - antecessor e sucessor

---
+++ ex005.py
@@ -0,0 +1,5 @@
+""" Programa recebe um número inteiro
+    E mostra o seu antecessor e seu sucessor.
+"""
+number = int(input('Digite um número inteiro: '))
+print(f'O antecessor de {number} é {number - 1}\n e seu sucessor é {number + 1}')
