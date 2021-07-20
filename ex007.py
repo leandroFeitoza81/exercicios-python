0915c44dec7e7bfe5fb75ce5f5f01ba9639726fe
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-07-19 23:31:19 -0300

    exercicio 07 - calcula média aritmética

---
+++ ex007.py
@@ -0,0 +1,7 @@
+"""
+    O programa lê dois números e calcula a média
+"""
+
+number1 = float(input("Digite o primeiro número "))
+number2 = float(input("Digite o segundo número "))
+print(f'A média aritmética é {(number1 + number2) / 2}')
