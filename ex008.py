8a3ce5f332a53e1179b034a06dff7d92fd4d9a3c
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-07-19 23:40:31 -0300

    exercicio 08 - conversor centimetro e milimetro

---
+++ ex008.py
@@ -0,0 +1,7 @@
+"""
+    Conversor de metros para centímetro e milimetro
+"""
+meter = float(input('Digite o valor em metros(m) ==>'))
+centimeters = meter * 100
+millimeter = meter * 1000
+print(f'{meter} tem {centimeters} centímetros e {millimeter} milimetros')
