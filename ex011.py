4939f69978d6204d0eade873fa4bb269a8605cd0
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-07-21 12:44:26 -0300

    exercicio 11 - Mostra o maior numero

---
+++ ex011.py
@@ -0,0 +1,9 @@
+"""
+    Programa recebe dois números e mostra o maior.
+"""
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+if num1 > num2:
+    print(f'O número {num1} é maior')
+else:
+    print(f'O número {num2} é maior')
