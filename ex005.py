7a6046c8d1d3657581f4545f2de52856c90e8e5a
Leandro Feitoza <leandro.feitoza8018@gmail.com>
2021-07-22 17:43:00 -0300

    exercicio 11 - refatorado tratamento de entrada de dados

--- ex005.py
+++ ex005.py
@@ -1,5 +1,12 @@
 """ Programa recebe um número inteiro
     E mostra o seu antecessor e seu sucessor.
 """
-number = int(input('Digite um número inteiro: '))
-print(f'O antecessor de {number} é {number - 1}\n e seu sucessor é {number + 1}')
+while True:
+    try:
+        number = int(input('Digite um número inteiro: '))
+    except ValueError:
+        print("Valor inválido ")
+    else:
+        break
+
+print(f'O antecessor de {number} é {number - 1} e seu sucessor é {number + 1}')
